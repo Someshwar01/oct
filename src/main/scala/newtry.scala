adc20f91268b05ea497df05b29cadd65b846cd7c
Someshwar01 <ingolesomu@gmail.com>
2024-10-19 10:41:04 +0530

    added new line

--- src/main/scala/newtry.scala
+++ src/main/scala/newtry.scala
@@ -1,13 +1,15 @@
 object newtry {
-  def main(args:Array[String]):Unit=
-  {
-    println("hello world")
-        println("added new line")
-    
+  def main(args:Array[String]):Unit= {
+
+
+
+    var tup=(1,2,3,4,5,7)
+for(i<-tup.productIterator){
+  println(i)
+}
 
 
   }
-
 }
 
 
