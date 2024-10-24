65c9ba60a8b7367e42c44b9aa9c8d0660c0e7dea
Someshwar01 <ingolesomu@gmail.com>
2024-10-24 21:08:13 +0530

    Combination of " ForLoop, IfConditional & Boolean "

--- src/main/scala/newtry.scala
+++ src/main/scala/newtry.scala
@@ -1,26 +1,39 @@
 import org.apache.spark.SparkContext
 
+
 object newtry {
-  def main(args:Array[String]):Unit= {
+  def main(args: Array[String]): Unit = {
 
 
+    //
+    // val sc=new SparkContext("local[2]","Someshwar")
+    // val input=sc.textFile("C:/Users/anike/Desktop/check.txt")
+    //    val rdd1=input.flatMap(x=>x.split(" "))
+    //    val rdd2=rdd1.map(x=>(x,1))
+    //    val rdd3=rdd2.reduceByKey((x,y)=>x+y)
+    //    val rdd4=rdd3.sortBy(x=>x._2,false)
+    //    rdd4.take(2).foreach(println)
+    //
+    //    scala.io.StdIn.readLine()
+    //
 
- val sc=new SparkContext("local[2]","Someshwar")
- val input=sc.textFile("C:/Users/anike/Desktop/check.txt")
-    val rdd1=input.flatMap(x=>x.split(" "))
-    val rdd2=rdd1.map(x=>(x,1))
-    val rdd3=rdd2.reduceByKey((x,y)=>x+y)
-    val rdd4=rdd3.sortBy(x=>x._2,false)
-    rdd4.take(6).foreach(println)
+    for (i <- 0 to 11) {
+      for (j <- 0 to 11) {
 
-    scala.io.StdIn.readLine()
+        if (i == 10 || j == 10 || i + j == 10) {
 
+          println("Result " + i, j + " : True")
+        }
+      }
 
 
+    }
 
   }
-
-
 }
+
+
+
+
 
 
