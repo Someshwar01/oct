2c23b14696d2e5ce9116f89343195a6c6fd35d2f
Someshwar01 <ingolesomu@gmail.com>
2024-10-24 08:08:57 +0530

    Spark check Spark is working properly

--- src/main/scala/newtry.scala
+++ src/main/scala/newtry.scala
@@ -1,15 +1,26 @@
+import org.apache.spark.SparkContext
+
 object newtry {
   def main(args:Array[String]):Unit= {
 
 
 
-    var tup=(1,2,3,4,5,7)
-for(i<-tup.productIterator){
-  println(i)
-}
+ val sc=new SparkContext("local[2]","Someshwar")
+ val input=sc.textFile("C:/Users/anike/Desktop/check.txt")
+    val rdd1=input.flatMap(x=>x.split(" "))
+    val rdd2=rdd1.map(x=>(x,1))
+    val rdd3=rdd2.reduceByKey((x,y)=>x+y)
+    val rdd4=rdd3.sortBy(x=>x._2,false)
+    rdd4.take(6).foreach(println)
+
+    scala.io.StdIn.readLine()
+
+
 
 
   }
+
+
 }
 
 
