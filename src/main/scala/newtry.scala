52db9889dffb84f68eece9278209964b9e6fe42a
Someshwar01 <ingolesomu@gmail.com>
2024-10-25 20:01:03 +0530

    practice sets, whileLoop, ForLoop, tuple

--- src/main/scala/newtry.scala
+++ src/main/scala/newtry.scala
@@ -17,17 +17,37 @@ object newtry {
     //    scala.io.StdIn.readLine()
     //
 
-    for (i <- 0 to 11) {
-      for (j <- 0 to 11) {
 
-        if (i == 10 || j == 10 || i + j == 10) {
+    //    for (i <- 0 to 11) {
+    //      for (j <- 0 to 11) {
+    //
+    //        if (i == 10 || j == 10 || i + j == 10) {
+    //
+    //          println("Result " + i, j + " : True")
+    //        }
+    //      }
+    //    }
 
-          println("Result " + i, j + " : True")
-        }
-      }
 
+    //    val tup =(10,"scala",true,45,10,12,10)
+    //    tup.productIterator.foreach(println)
+    val set = Set(10, 20, 3, 5, 80, 40, 50)
 
+    for (el <- set) {
+
+      println(el)
     }
+
+    //    val es =set.contains(3)
+    //    print(es)
+    //  print(tup._4)
+
+    var op = 55
+    while (op <= 67) {
+      print(op +"  ")
+      op+=1
+    }
+
 
   }
 }
